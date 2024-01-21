14dbcb4244634e2881577faddc50609d3e95821c
Bader <baljishi408@gmail.com>
2024-01-21 11:55:32 -0800

    Added keybinding for easily printing full filepath or current open in neovim

--- mappings.lua
+++ mappings.lua
@@ -8,7 +8,10 @@ M.general = {
     ["<C-q>"] = { "<C-W>q", "Quit current window" },
     ["<C-r>"] = { "<C-W>r", "Rotate windows downwards N times"},
     ["<C-s>"] = { "<C-W>s", "Split the current window into two parts, new Window N lines high"},
-    ["<C-v>"] = { "<C-W>v", "Split the current window vertically, new window N colums wide"}
+    ["<C-v>"] = { "<C-W>v", "Split the current window vertically, new window N colums wide"},
+
+    -- Keybinds to make common tasks easier to call
+    ["<C-g>"] = { "1<C-G>", "Show the full filepath of the file currently opened"}
   },
 }
 
